cdf4d1aea43e66945fb84b944f46469238ee5145
vnychka_sviashennika <vnychkasviashennika@yandex.ru>
2025-02-06 15:30:21 +0300

    Добавьте файлы проекта.

---
+++ UserClasses/InfoEmailSending.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr3.UserClasses
+{
+    public class InfoEmailSending
+    {
+        public InfoEmailSending(string smtpClientAdress,
+                                StringPair emailAdressFrom,
+                                string emailPassword,
+                                StringPair emailAdressTo,
+                                string subject,
+                                string body)
+        {
+            SmtpClientAdress = String.IsNullOrWhiteSpace(smtpClientAdress) ?
+                throw new Exception("Нельзя вставлять пробелы или пустое значение!") :
+                smtpClientAdress;
+
+            EmailAdressFrom = emailAdressFrom ?? throw new ArgumentNullException(nameof(emailAdressFrom));
+
+            EmailPassword = String.IsNullOrWhiteSpace(emailPassword) ?
+                throw new Exception("Нельзя вставлять пробелы или пустое значение!") :
+                emailPassword;
+
+            EmailAdressTo = emailAdressTo ?? throw new ArgumentNullException(nameof(emailAdressTo));
+
+            Subject = subject ?? throw new ArgumentNullException(nameof(subject));
+
+            Body = body ?? throw new ArgumentNullException(nameof(body));
+        }
+
+        public string SmtpClientAdress { get; set; }
+
+        public StringPair EmailAdressFrom { get; set; }
+
+        public string EmailPassword { get; set; }
+
+        public StringPair EmailAdressTo { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+    }
+}
+public class StringPair
+{
+    public StringPair(string emailAdress, string name)
+    {
+        EmailAdress = String.IsNullOrWhiteSpace(emailAdress) ?
+            throw new Exception("Нельзя вставлять пробелы или пустое значение!") :
+            emailAdress;
+
+        Name = String.IsNullOrWhiteSpace(name) ?
+            throw new Exception("Нельзя вставлять пробелы или пустое значение!") :
+            name;
+    }
+
+    public string EmailAdress { get; set; }
+    public string Name { get; set; }
+}
+
+
---
+++ UserClasses/SendingEmail.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace pr3.UserClasses
+{
+    public class SendingEmail
+    {
+        private InfoEmailSending InfoEmailSending { get; set; }
+
+        public SendingEmail(InfoEmailSending infoEmailSending)
+        {
+            InfoEmailSending = infoEmailSending
+                ?? throw new ArgumentNullException(nameof(infoEmailSending));
+        }
+    }
+
+    public void Send()
+    {
+        // Добавляем обработку исключений
+        try
+        {
+            // Вносим адрес SMTP сервера
+            SmtpClient mySmtpClient =
+                new SmtpClient(InfoEmailSending.SmtpClientAdress);
+
+            // Задаём учетные данные пользователя
+            mySmtpClient.UseDefaultCredentials = false;
+            // Включаем использование протокола SSL
+            mySmtpClient.EnableSsl = true;
+
+            // Задаём учетные данные пользователя
+            NetworkCredential basicAuthenticationInfo = new NetworkCredential(
+                InfoEmailSending.EmailAdressFrom.EmailAdress,
+                InfoEmailSending.EmailAdressFrom.EmailPassword);
+
+            mySmtpClient.Credentials = basicAuthenticationInfo;
+
+            // Добавляем адрес откуда отправляемое сообщение
+            MailAddress from = new MailAddress(
+                InfoEmailSending.EmailAdressFrom.EmailAdress,
+                InfoEmailSending.EmailAdressFrom.Name);
+
+            // Добавляем адрес куда будет отправлено сообщение
+            MailAddress to = new MailAddress(
+                InfoEmailSending.EmailAdressTo.EmailAdress,
+                InfoEmailSending.EmailAdressTo.Name);
+
+            MailMessage myMail = new MailMessage(from, to);
+
+            // Добавляем наш адрес в список адресов для ответа
+            MailAddress replyTo =
+                new MailAddress(InfoEmailSending.EmailAdressFrom.EmailAdress);
+            myMail.ReplyToList.Add(replyTo);
+
+            // Выбираем кодировку символов в письме
+            // В нашем случае UTF8
+            Encoding encoding = Encoding.UTF8;
+            // Задаём значение Заголовка и его кодировку
+            myMail.Subject = InfoEmailSending.Subject;
+            myMail.SubjectEncoding = encoding;
+
+            // Задаём значение Сообщения и его кодировку
+            myMail.Body = InfoEmailSending.Body;
+            myMail.BodyEncoding = encoding;
+
+            // Отправляем письмо
+            mySmtpClient.Send(myMail);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+
+}
